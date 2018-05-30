82f312d6d488808887b01be5a128fa367bde2fe7
Ricardo Constantino <constantphp@gmail.com>
2018-05-30 00:16:06 -0300

    add function find items price and some this values

--- app/repositories/Items.php
+++ app/repositories/Items.php
@@ -2,6 +2,7 @@
 
 namespace Repositories;
 
+use Illuminate\Database\Capsule\Manager as DB;
 use Model\Items as ItemsModel;
 
 class Items
@@ -11,6 +12,23 @@ class Items
     {
         $items = ItemsModel::all("id", "descricao")->all();
         return $items;
+    }
+
+    public static function findPrice($item_id) : string
+    {
+        $preco = "";
+
+        $items = DB::table("items")
+            ->select("preco")
+            ->whereIn("id", $item_id)
+            ->get();
+
+        foreach ($items as $item) {
+            $preco += $item->preco;
+        }
+
+        return $preco;
+
     }
 
 }
